6ca6337104bd4766b47bb08ee6b49b559519fa75
webdevno24 <webdevno24@gmail.com>
2021-03-18 13:08:07 +0700

    Udapte: custom report (post body) sections

--- app/Traits/PefindoTrait.php
+++ app/Traits/PefindoTrait.php
@@ -183,8 +183,8 @@ trait PefindoTrait
                             <arr:string>CIP</arr:string>
                             <!-- <arr:string>SubjectInfoHistory</arr:string> -->
                             <arr:string>ContractList</arr:string>
-                            <arr:string>ContractSummary</arr:string>
-                            <!-- <arr:string>SubjectInfo</arr:string> -->
+                            <!-- <arr:string>ContractSummary</arr:string> -->
+                            <arr:string>SubjectInfo</arr:string>
                         </cus:Sections>
                         <cus:SubjectType>Company</cus:SubjectType>
                     </cb5:parameters>
